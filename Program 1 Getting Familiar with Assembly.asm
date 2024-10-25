f1cc6dcdf72abcc7a968085f19a47344a6cc2c07
Jonathan Dang <79808820+jdang9191@users.noreply.github.com>
2024-10-25 10:39:42 -0700

    Update Program 1 Getting Familiar with Assembly.asm
    added add, sub, mul, div, however still need to implement menu because right now all of the arithmetic cases are displayed

--- Program 1 Getting Familiar with Assembly.asm
+++ Program 1 Getting Familiar with Assembly.asm
@@ -1,11 +1,12 @@
 # CS 2640
 # 10/21/2024
 # Program 1 Getting Familiar with Assembly
-# Brandon Tseng, Michael Wu, 
+# Brandon Tseng, Michael Wu, Jonathan Dang
 
 .data
 	prompt1: .asciiz		"What is your first number: "
 	prompt2: .asciiz		"What is your second number: "
+	menu: .asciiz "1) addition\n2) Subtraction\n3) Multiplication\n4)Division\n"
 
 .text
 main:
@@ -28,6 +29,30 @@ main:
 	li $v0, 5
 	syscall
 	move $s1, $v0
+
+	#adding 2 ints
+	add $s3, $s0, $s1
+	li $v0, 1
+	move $a0, $s3
+	syscall
+
+	#subtracting 2 ints
+	sub $s3, $s0, $s1
+	li $v0, 1
+	move $a0, $s3
+	syscall
+
+	#multiplying 2 ints
+	mul $s3, $s0, $s1
+	li $v0, 1
+	move $a0, $s3
+	syscall
+
+	#dividing 2 ints
+	div $s3, $s0, $s1
+	li $v0, 1
+	move $a0, $s3
+	syscall
 	
 	# Exit syscall
 	li $v0, 10
